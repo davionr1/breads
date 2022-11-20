c9b092f4b4307407b924713855aeea5fae49a79c
Davion Roachford <davionr1@icloud.com>
2022-11-20 10:43:28 -0800

    create and new routes complete

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
 // DEPENDENCIES
 const express = require('express')
 
@@ -11,18 +6,69 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
+  res.send('Welcome to an Awesome App about Breads')
+})
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
 
+
+
+// // DEPENDENCIES
+// const express = require('express')
+// const mongoose = require('mongoose')
+
+// // CONFIGURATION
+// require('dotenv').config()
+// const PORT = process.env.PORT
+// const app = express()
+// // Breads
+//   const breadsController = require('./controllers/breads_controller.js')
+//   app.use('/breads', breadsController)
+// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+//   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+// )
+
+// // MIDDLEWARE
+// app.set('views', __dirname + '/views')
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+
+
+// // ROUTES
+// app.get('/', (req, res) => {
+//     res.send('Welcome to an Awesome App about Breads')
+//   })
+  
+  
+  
+// // app.get('*', (req,res)=>{
+// //   res.send('error404')
+// // })
+
+// // LISTEN
+// app.listen(PORT, () => {
+//   console.log('listening on port', PORT);
+// })
--- views/index.jsx
+++ views/index.jsx
@@ -1,12 +1,12 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index ({breads})  {
+function Index ({breads, title})  {
     return (
-        <Default>
+        <Default title={title}>
   <h2>Index Page</h2>
-  {/* <p>I have {breads[0].name} bread!</p> */}
-  {/* This is a JSX comment. */}
+  {<p>I have {breads[0].name} bread!</p>}
+  
   <ul>
   {
   breads.map((bread, index)=> {
@@ -21,6 +21,14 @@ function Index ({breads})  {
 }
 
   </ul>
+  <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+
+<div className="backButton">
+  <a href="/breads"><button>Go back to the index</button></a>
+</div>
+
 </Default>   
     )
 }
